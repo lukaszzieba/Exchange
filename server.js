602c6b4b35bbcaeb387e677b6b2719e09bd2128a
Finch <lukaszzieba4@gmail.com>
2016-06-07 11:23:10 +0100

    Server init

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     logger = require('morgan'),
-    favicon = require('serve-favicon');
+    favicon = require('serve-favicon'),
+    config = require('./server/config')
 
 var app = express();
 
-app.use(express.static(__dirname + '/client'));
-app.use(favicon(__dirname + '/client/img/favicon-16x16.png'));
+app.use(express.static(config.rootPath + '/client'));
+app.use(favicon(config.rootPath + '/client/img/favicon-16x16.png'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/currencies', function(req, res) {
+app.use(require('./server/account.js'));
 
-});
-
-app.listen(8080, function() {
+app.listen(config.port, function() {
     console.log('Listening on port ' + 8080);
 });
---
+++ server/config.js
@@ -0,0 +1,8 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..')
+
+module.exports = {
+  rootPath : rootPath,
+  port: 8080,
+  secret: 'asdqwezxc'
+};
