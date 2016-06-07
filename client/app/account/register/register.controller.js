c824a7315055cc5d138907ba608bd4f39de30493
Finch <lukaszzieba4@gmail.com>
2016-06-07 18:42:49 +0100

    Regisre user basic

--- client/app/account/register/register.controller.js
+++ client/app/account/register/register.controller.js
@@ -4,17 +4,28 @@
     angular
         .module('account.module')
         .controller('RegisterController', RegisterController);
-    RegisterController.$inject = ['$scope'];
+    RegisterController.$inject = ['$scope', 'RegisterService', 'store', 'IdentyService'];
 
-    function RegisterController($scope) {
+    function RegisterController($scope, RegisterService, store, IdentyService) {
 
 
-        $scope.signupForm = function() {
+        $scope.signupForm = function(newUser) {
             $scope.signup_form.submitted = false;
             console.log($scope.signup_form.$valid);
             if ($scope.signup_form.$valid) {
                 // Submit
-                console.log('Valid');
+
+                RegisterService.registerUser(newUser).
+                then(function(response) {
+                    store.set('jwt', response.data.id_token);
+                    var t = IdentyService.getDecodedToken();
+                    IdentyService.currentUser = t;
+                    console.log(t);
+                }, function(err) {
+                    console.log(err);
+                });
+
+
             } else {
                 $scope.signup_form.submitted = true;
             }
