9b10566e292a15631a20614f39cbec5427017bf8
Finch <lukaszzieba4@gmail.com>
2016-06-07 15:15:27 +0100

    Register page basic styling with validation.

--- client/app/account/register/register.controller.js
+++ client/app/account/register/register.controller.js
@@ -4,11 +4,21 @@
     angular
         .module('account.module')
         .controller('RegisterController', RegisterController);
-    RegisterController.$inject = [];
+    RegisterController.$inject = ['$scope'];
 
-    function RegisterController() {
-        var vm = this;
+    function RegisterController($scope) {
 
+
+        $scope.signupForm = function() {
+            $scope.signup_form.submitted = false;
+            console.log($scope.signup_form.$valid);
+            if ($scope.signup_form.$valid) {
+                // Submit
+                console.log('Valid');
+            } else {
+                $scope.signup_form.submitted = true;
+            }
+        }
         activate();
 
         function activate() {
