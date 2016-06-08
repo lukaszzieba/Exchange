d34ba19e15e1d50e7722de5829509ceb40c04713
Finch <lukaszzieba4@gmail.com>
2016-06-08 17:46:42 +0100

    Wallet quite ok

--- client/app/wallet/wallet.contorller.js
+++ client/app/wallet/wallet.contorller.js
@@ -4,16 +4,17 @@
     angular
         .module('exchangeApp')
         .controller('WalletController', WalletController);
-    WalletController.$inject = ['WalletService'];
+    WalletController.$inject = ['WalletService', 'CurrencyService', '$scope'];
 
-    function WalletController(WalletService) {
+    function WalletController(WalletService, CurrencyService, $scope) {
         var vm = this;
         vm.userWallet = WalletService
+        vm.currencies = CurrencyService.collection
         activate();
 
         function activate() {
             return getWallet()
-                .then(function() {                  
+                .then(function() {
                     console.log('Activate wallet');
                 });
         }
@@ -21,9 +22,22 @@
         function getWallet() {
             return WalletService.getWallet()
                 .then(function(data) {
-                  vm.wallet = data;
+                    vm.wallet = data;
                     return vm.wallet;
                 });
         }
+
+        // getting current sell price
+        vm.getUintPrice = function(cur) {
+            var price = 0;
+            vm.currencies[0].Items.forEach(function(item) {
+                if (item.Code === cur.code) {
+                    price = item.SellPrice;
+                    console.log(price);
+                }
+            });
+            return price;
+        }
+        
     }
 }());
